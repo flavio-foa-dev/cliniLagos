60602f3ed3f1bf2d063d1359c80e0d2cdecdb3d9
Flavio Andrade <foa12381@gmail.com>
2023-04-24 18:11:05 -0300

    add page login

---
+++ cliniLagos/src/components/Login/Login.tsx
@@ -0,0 +1,16 @@
+
+
+export default function Login() {
+  return (
+    <div>Login
+      <form>
+        <label htmlFor="nome">Nome</label>
+        <input type="text" id="nome" value="" />
+        <label htmlFor="email"></label>
+        <input type="email" name="" id="email" />
+        <label htmlFor="password"></label>
+        <input type="password" name="" id="password" />
+      </form>
+    </div>
+  )
+}
